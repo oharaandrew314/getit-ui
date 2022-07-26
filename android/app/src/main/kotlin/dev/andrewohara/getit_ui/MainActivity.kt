01d48e67bd4ce78a07ead0efc8da58e55518edb8
aohara <aohara@camcloud.com>
2022-07-25 20:48:52 -0400

    initial commit

---
+++ android/app/src/main/kotlin/dev/andrewohara/getit_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.andrewohara.getit_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/list_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class ListView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
