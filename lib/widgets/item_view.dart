c40987477fd85836749756a025d6ecd5f2a953ec
aohara <aohara@camcloud.com>
2022-07-31 19:25:33 -0400

    :lipstick: improve item editing ux

--- lib/widgets/item_view.dart
+++ lib/widgets/item_view.dart
@@ -1,68 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:getit_ui/client/dtos.dart';
 
-class ItemView extends StatefulWidget {
+class Item extends StatelessWidget {
   final ItemDto item;
-  final Function(ItemDataDto) update;
-  final Function() delete;
 
+  final VoidCallback select;
+  final Function(bool?) check;
 
-  const ItemView({
+  const Item({
     required this.item,
-    required this.delete,
-    required this.update, Key? key
+    required this.select,
+    required this.check,
+    Key? key
   }): super(key: key);
 
   @override
-  State<StatefulWidget> createState() => _ItemViewState();
+  Widget build(BuildContext context) {
+    final content = Row(
+        children: [
+          Expanded(
+              child: Checkbox(
+                value: item.completed,
+                onChanged: check,
+              )
+          ),
+          Expanded(
+              flex: 10,
+              child: Text(item.name)
+          ),
+        ]
+    );
+
+    return InkWell(
+      child: content,
+      onTap: select
+    );
+  }
 }
 
-class _ItemViewState extends State<ItemView> {
-  final _controller = TextEditingController();
+class EditableItem extends StatelessWidget {
+  final ItemDto item;
+
+  final Function(ItemDataDto) update;
+  final Function() delete;
+  final VoidCallback deselect;
+
+  const EditableItem({
+    required this.item,
+    required this.update,
+    required this.delete,
+    required this.deselect,
+    Key? key
+  }): super(key: key);
 
   void _handleCheck(bool? completed) {
-    final data = ItemDataDto(name: widget.item.name, completed: completed ?? false);
-    widget.update(data);
+    final data = ItemDataDto(name: item.name, completed: completed ?? false);
+    update(data);
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _controller.text = widget.item.name;
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
+  void _handleRename(String name) {
+    final data = ItemDataDto(name: name, completed: item.completed);
+    update(data);
+    deselect();
   }
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: [
-        Expanded(
-          child: Checkbox(
-            value: widget.item.completed,
-            onChanged: _handleCheck,
+        children: [
+          Expanded(
+              child: Checkbox(
+                value: item.completed,
+                onChanged: _handleCheck,
+              )
+          ),
+          Expanded(
+              flex: 10,
+              child: TextFormField(
+                initialValue: item.name,
+                autofocus: true,
+                onFieldSubmitted: _handleRename
+              )
+          ),
+          Expanded(
+              child: IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () => delete(),
+              )
           )
-        ),
-        Expanded(
-          flex: 10,
-          child: SizedBox(
-            width: 100,
-            child: TextField(
-              controller: _controller
-            )
-          )
-        ),
-        Expanded(
-          child: IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: () => widget.delete(),
-          )
-        )
-      ]
+        ]
     );
   }
 }
--- lib/widgets/list_items_view.dart
+++ lib/widgets/list_items_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:getit_ui/client/dtos.dart';
 import 'package:getit_ui/widgets/item_view.dart';
 
-class ListItemsView extends StatelessWidget {
+class ListItemsView extends StatefulWidget {
   final List<ItemDto> items;
   final Function(ItemDto, ItemDataDto) updateItem;
   final Function(ItemDto) deleteItem;
@@ -17,21 +17,55 @@ class ListItemsView extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() => _ListItemsViewState();
+}
+
+class _ListItemsViewState extends State<ListItemsView> {
+
+  ItemDto? _selected;
+
+  void _select(ItemDto? item) {
+    setState(() => _selected = item);
+  }
+
+  void _check(ItemDto item, bool completed) {
+    final data = ItemDataDto(name: item.name, completed: completed);
+    widget.updateItem(item, data);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return RefreshIndicator(
         child: ListView.builder(
-          scrollDirection: Axis.vertical,
-          itemCount: items.length,
-          itemBuilder: (BuildContext context, int index) {
-            final item = items[index];
-            return ItemView(
-              item: item,
-              update: (data) => updateItem(item, data),
-              delete: () => deleteItem(item),
-            );
-          }
+            scrollDirection: Axis.vertical,
+            itemCount: widget.items.length,
+            itemBuilder: (BuildContext context, int index) {
+              final item = widget.items[index];
+              return _selected == item
+                ? EditableItem(
+                  item: item,
+                  update: (data) => widget.updateItem(item, data),
+                  delete: () => widget.deleteItem(item),
+                  deselect: () => _select(null)
+                )
+                : Item(
+                  item: item,
+                  select: () => _select(item),
+                  check: (completed) => _check(item, completed ?? false),
+                )
+                ;
+
+              // return ItemView(
+              //   item: item,
+              //   update: (data) => widget.updateItem(item, data),
+              //   delete: () => widget.deleteItem(item),
+              //   selected: _selected == item,
+              //   select: (select) => _select(select ? item : null),
+              // );
+            }
         ),
-        onRefresh: () async { refreshItems?.call(); }
+        onRefresh: () async { widget.refreshItems?.call(); }
     );
   }
+
 }
